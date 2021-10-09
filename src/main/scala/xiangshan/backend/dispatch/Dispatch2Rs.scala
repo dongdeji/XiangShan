6f5efe1bbec4ef903c27c891fc1c83ecb2c147e1
Yinan Xu <xuyinan@ict.ac.cn>
2021-10-09 16:45:21 +0800

    core: update dispatch port parameters

--- src/main/scala/xiangshan/backend/dispatch/Dispatch2Rs.scala
+++ src/main/scala/xiangshan/backend/dispatch/Dispatch2Rs.scala
@@ -37,7 +37,7 @@ class Dispatch2Rs(val configs: Seq[Seq[ExuConfig]])(implicit p: Parameters) exte
 
   // Different mode of dispatch
   // (1) isDistinct: no overlap
-  val isDistinct = exuConfigCases.flatMap(_._1).distinct.length == exuConfigCases.flatMap(_._1).length
+  val isDistinct = exuConfigCases.flatMap(_._1).distinct.length == exuConfigCases.flatMap(_._1).length && exuConfigCases.length > 1
   // (2) isLessExu: exu becomes less and less
   val isSubset = configs.dropRight(1).zip(configs.tail).forall(x => x._2.toSet.subsetOf(x._1.toSet))
   val isLessExu = configs.length > 1 && isSubset
